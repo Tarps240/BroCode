ca7e661a417f6f5f7289e5207842a479082c18fe
Tarps240 <sl3966@gmail.com>
2024-10-27 08:29:06 -0500

    initial commit

---
+++ bro-code-css-styling/src/App.jsx
@@ -0,0 +1,13 @@
+// React hook = Special function that allows functional components      to use React features without writing class components (React v16.8) (useState, useEffect, useContext, useReducer, useCallback, and more...)
+
+// useState() = A React hook that allows the creation of a stateful variable and a setter function to update its value in the virtual DOM.
+// [name, setName]
+
+
+
+function App() {
+
+    return(<></>);
+}
+
+export default App

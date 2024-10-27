6da9f3efda6d3d2be04c29cdd40a5894ee037a3e
Tarps240 <sl3966@gmail.com>
2024-10-27 07:53:07 -0500

    Finished Card Component Commit

--- card-component/src/Card.jsx
+++ card-component/src/Card.jsx
@@ -1,7 +1,11 @@
-
-
 function Card () {
-
-}
+    return (
+        <div className="card">
+            <img className = "card-image" src="https://placeholdr.ai/15752bb1-e469-4c07-a88d-45a81823c4a9/200/200" alt="Profile-picture" />
+            <h2 className = "card-title">Scottie</h2>
+            <p className = "card-text">I am learning to code and I play Ice Hockey</p>
+        </div>
+    );
+};
 
 export default Card;

97a74a2075132fda35851cc4b3ad78bdda04f10d
Tarps240 <sl3966@gmail.com>
2024-11-02 14:06:32 -0500

    Training commit v1

---
+++ bro-code-clickEvents/src/App.jsx
@@ -0,0 +1,12 @@
+// Click Event = An interaction when a user clicks on a specific elememnt. We can respond to clicks by passing a callback to the onClick event handler.
+
+import Button from './Button';
+
+
+function App() {
+  return(
+    <Button />
+  );
+}
+
+export default App
---
+++ bro-code-clickEvents/src/Button.jsx
@@ -0,0 +1,14 @@
+
+
+function Button() {
+
+    const handleClick = () => {
+        console.log("Ouch!");
+    }
+
+    return(
+        <button onClick = {handleClick}>Click Me!😁</button>
+    );
+};
+
+export default Button
